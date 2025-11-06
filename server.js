19aec732f23130c36e1e61dd905a5527559d61d3
roni shil <roniyouknow@gmail.com>
2025-11-06 19:02:39 +0530

    update .env

--- server.js
+++ server.js
@@ -83,4 +83,5 @@ app.post("/submit-form", async (req, res) => {
   }
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
